be876b083140ab6b78a23c857f310c64522cddeb
Marcus Ruddick <ruddickmg@gmail.com>
2019-09-16 23:15:39 -0700

    working through app configuration

--- api-server/src/api.ts
+++ api-server/src/api.ts
@@ -2,6 +2,6 @@ import express, { Request, Response } from 'express';
 
 const app = express();
 
-app.get('/', (req: Request, res: Response): void => res.send('hello world'));
+app.get('/', (req: Request, res: Response): Response => res.send('hello world'));
 
 app.listen();
