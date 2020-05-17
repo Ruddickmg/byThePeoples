d03eba7075330afef30fa4d56762b3270778c9ba
Marcus Ruddick <ruddickmg@gmail.com>
2020-05-17 13:34:51 -0700

    tested verification handler, added missing lib

---
+++ lib/status_codes/src/lib.rs
@@ -0,0 +1,12 @@
+pub type StatusCode = u16;
+
+pub const OKAY: StatusCode = 200;
+pub const CREATED: StatusCode = 201;
+pub const ACCEPTED: StatusCode = 202;
+pub const BAD_REQUEST: StatusCode = 400;
+pub const UNAUTHORIZED: StatusCode = 401;
+pub const FORBIDDEN: StatusCode = 403;
+pub const NOT_FOUND: StatusCode = 404;
+pub const CONFLICT: StatusCode = 409;
+pub const UNPROCESSABLE_ENTITY: StatusCode = 422;
+pub const INTERNAL_SERVER_ERROR: StatusCode = 500;
--- services/auth/src/handler/verification.rs
+++ services/auth/src/handler/verification.rs
@@ -30,24 +30,67 @@ pub async fn authenticate_credentials<
 #[cfg(test)]
 mod verification_handler_test {
     use super::*;
-    use crate::utilities::test::fake;
+    use crate::{controller::password, utilities::test::fake, Error};
     use actix_rt;
+    use actix_web::{http, web};
 
     #[actix_rt::test]
-    async fn returns_okay_on_successful_authentication() {}
+    async fn returns_okay_on_successful_authentication() {
+        let mut state = fake::service_state();
+        let request = fake::name_request();
+        let mut record = fake::credentials();
+        record.hash = password::hash_password(&request.password).unwrap();
+        state.credentials.by_name.returns(Some(record.clone()));
+        let result = authenticate_credentials(web::Data::new(state), web::Json(request)).await;
+        assert_eq!(result.status(), status_codes::OKAY);
+    }
 
     #[actix_rt::test]
-    async fn sets_auth_header_on_successful_authentication() {}
+    async fn sets_auth_header_on_successful_authentication() {
+        let mut state = fake::service_state();
+        let request = fake::name_request();
+        let mut record = fake::credentials();
+        record.hash = password::hash_password(&request.password).unwrap();
+        state.credentials.by_name.returns(Some(record.clone()));
+        let result = authenticate_credentials(web::Data::new(state), web::Json(request)).await;
+        assert!(result.headers().contains_key(http::header::AUTHORIZATION));
+    }
 
     #[actix_rt::test]
-    async fn returns_unauthorized_on_failed_authentication() {}
+    async fn returns_unauthorized_on_failed_authentication() {
+        let mut state = fake::service_state();
+        let request = fake::name_request();
+        state.credentials.by_name.returns(None);
+        let result = authenticate_credentials(web::Data::new(state), web::Json(request)).await;
+        assert_eq!(result.status(), status_codes::UNAUTHORIZED);
+    }
 
     #[actix_rt::test]
-    async fn does_not_set_auth_header_on_failed_authentication() {}
+    async fn does_not_set_auth_header_on_failed_authentication() {
+        let mut state = fake::service_state();
+        let request = fake::name_request();
+        state.credentials.by_name.returns(None);
+        let result = authenticate_credentials(web::Data::new(state), web::Json(request)).await;
+        assert!(!result.headers().contains_key(http::header::AUTHORIZATION));
+    }
 
     #[actix_rt::test]
-    async fn returns_internal_server_error_on_unexpected_error() {}
+    async fn returns_internal_server_error_on_unexpected_error() {
+        let error = Error::InternalServerError("testing".to_string());
+        let mut state = fake::service_state();
+        let request = fake::name_request();
+        state.credentials.by_name.throws_error(error);
+        let result = authenticate_credentials(web::Data::new(state), web::Json(request)).await;
+        assert_eq!(result.status(), status_codes::INTERNAL_SERVER_ERROR);
+    }
 
     #[actix_rt::test]
-    async fn does_not_set_auth_header_on_unexpected_error() {}
+    async fn does_not_set_auth_header_on_unexpected_error() {
+        let error = Error::InternalServerError("testing".to_string());
+        let mut state = fake::service_state();
+        let request = fake::name_request();
+        state.credentials.by_name.throws_error(error);
+        let result = authenticate_credentials(web::Data::new(state), web::Json(request)).await;
+        assert!(!result.headers().contains_key(http::header::AUTHORIZATION));
+    }
 }
