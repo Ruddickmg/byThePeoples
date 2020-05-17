30fa78d8daf2587c90729359ae36a3fe4e2c7158
Marcus Ruddick <ruddickmg@gmail.com>
2020-05-17 14:14:41 -0700

    tested delete credentials handler

--- services/auth/src/handler/credentials/deletion.rs
+++ services/auth/src/handler/credentials/deletion.rs
@@ -19,3 +19,42 @@ pub async fn delete_credentials<
     }
     .finish()
 }
+
+#[cfg(test)]
+mod delete_credentials_handler_test {
+    use super::*;
+    use crate::{controller::password, utilities::test::fake, Error};
+    use actix_rt;
+    use actix_web::web;
+
+    #[actix_rt::test]
+    async fn returns_accepted_on_successful_deletion() {
+        let mut state = fake::service_state();
+        let request = fake::email_request();
+        let mut record = fake::credentials();
+        record.hash = password::hash_password(&request.password).unwrap();
+        state.credentials.by_email.returns(Some(record.clone()));
+        state.credentials.mark_as_deleted_by_email.returns(1);
+        let result = delete_credentials(web::Data::new(state), web::Json(request)).await;
+        assert_eq!(result.status(), status_codes::ACCEPTED);
+    }
+
+    #[actix_rt::test]
+    async fn returns_unauthorized_on_failed_authentication() {
+        let mut state = fake::service_state();
+        let request = fake::email_request();
+        state.credentials.by_email.returns(None);
+        let result = delete_credentials(web::Data::new(state), web::Json(request)).await;
+        assert_eq!(result.status(), status_codes::UNAUTHORIZED);
+    }
+
+    #[actix_rt::test]
+    async fn returns_internal_server_error_on_unexpected_error() {
+        let error = Error::InternalServerError("testing".to_string());
+        let mut state = fake::service_state();
+        let request = fake::email_request();
+        state.credentials.by_email.throws_error(error);
+        let result = delete_credentials(web::Data::new(state), web::Json(request)).await;
+        assert_eq!(result.status(), status_codes::INTERNAL_SERVER_ERROR);
+    }
+}
