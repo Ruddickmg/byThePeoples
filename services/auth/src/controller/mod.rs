c9d6bcf598980eaf4270db18325afb3f94ee6233
ruddickmg <ruddickmg@gmail.com>
2020-05-24 11:56:23 -0700

    removed example

--- services/auth/src/controller/mod.rs
+++ services/auth/src/controller/mod.rs
@@ -2,36 +2,3 @@
 pub mod credentials;
 pub mod jwt;
 pub mod password;
-
-
-
-
-#[cfg(test)]
-mod example {
-    use super::*;
-    use actix_rt;
-    use serde::export::PhantomData;
-    struct Phantom;
-    struct InnerThing<'a> {
-        phantom: PhantomData<&'a Phantom>,
-    }
-
-    impl<'a> InnerThing<'a> {
-        async fn create() -> InnerThing<'a> {
-            InnerThing {
-                phantom: PhantomData,
-            }
-        }
-    }
-
-    struct Thing1<'a> {
-        thing: InnerThing<'a>,
-    }
-    trait Thing<'a> {}
-    impl<'a> Thing<'a> for Thing1 {}
-
-    #[actix_rt::test]
-    async fn example_of_problem() {
-
-    }
-}
